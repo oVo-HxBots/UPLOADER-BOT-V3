0ab7e5a06116349baaf4959f7e0d830f64136832
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2024-03-06 17:32:32 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -121,7 +121,7 @@ async def echo(bot, update):
             chat_id=update.chat.id,
             text=f'<b>Processing... ⏳</b>',
             disable_web_page_preview=True,
-            reply_to_message_id=update.message_id
+            reply_to_message_id=update.id
           )
     process = await asyncio.create_subprocess_exec(
         *command_to_exec,
@@ -143,8 +143,8 @@ async def echo(bot, update):
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.NO_VOID_FORMAT_FOUND.format(str(error_message)),
-            reply_to_message_id=update.message_id,
-            parse_mode="html",
+            reply_to_message_id=update.id,
+            parse_mode=enums.ParseMode.MARKDOWN,
             disable_web_page_preview=True
         )
         return False
@@ -252,8 +252,8 @@ async def echo(bot, update):
             chat_id=update.chat.id,
             text=Translation.FORMAT_SELECTION.format(Thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
             reply_markup=reply_markup,
-            parse_mode="html",
-            reply_to_message_id=update.message_id
+            parse_mode=enums.ParseMode.MARKDOWN,
+            reply_to_message_id=update.id
         )
     else:
         # fallback for nonnumeric port a.k.a seedbox.io
@@ -274,6 +274,6 @@ async def echo(bot, update):
             chat_id=update.chat.id,
             text=Translation.FORMAT_SELECTION,
             reply_markup=reply_markup,
-            parse_mode="html",
-            reply_to_message_id=update.message_id
+            parse_mode=enums.ParseMode.MARKDOWN,
+            reply_to_message_id=update.id
         )
